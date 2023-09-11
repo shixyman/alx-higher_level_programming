c97546c91a43a79d31de33620fb0375e03dccf50
shixyman <shixymanflowx@gmail.com>
2023-09-11 07:54:16 -0700

    modify

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
-    len_b = len(tuple_a)
+    len_b = len(tuple_b)
 
     if len_a == 0:
         a1 = 0
@@ -11,7 +11,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a2 = 0
     else:
         a1 = tuple_a[0]
-        a1 = tuple_a[1]
+        a2 = tuple_a[1]
 
     if len_b == 0:
         b1 = 0
@@ -21,8 +21,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b2 = 0
     else:
         b1 = tuple_b[0]
-        b2 = tupe_b[1]
+        b2 = tuple_b[1]
 
-    new_tuple = (a1 + b, a2 + b2)
+    new_tuple = (a1 + b1, a2 + b2)
 
     return (new_tuple)
