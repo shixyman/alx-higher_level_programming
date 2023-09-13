7e599e4f59cfa29083e12ad2af33075807e228d9
shixyman <shixymanflowx@gmail.com>
2023-09-13 07:37:16 -0700

    lodify

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**x, matrix[i]))
-
-    return (new_matrix)
+    for i in range(len(new_matrix)):
+        new_matrix[i] = list(map(lambda x: x ** x, new_matrix[i]))
+    return new_matrix
