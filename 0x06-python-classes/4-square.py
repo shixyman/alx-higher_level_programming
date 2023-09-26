1a1eabfa1bf9f845baca760a66d63a08e56e64a0
shixyman <shixymanflowx@gmail.com>
2023-09-26 07:18:19 -0700

    modify

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Defines a class Square in python"""
 
 
 class Square:
-    """Represents a square
-    Attributes:
+    """ square class :
         __size (int): size of a side of the square
     """
     def __init__(self, size=0):
