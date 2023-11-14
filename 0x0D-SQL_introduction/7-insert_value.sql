ddaee13986cc328263c308e42780f7140d5cb20c
shixyman <shixymanflowx@gmail.com>
2023-11-14 06:28:46 -0800

    modify

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -4,7 +4,7 @@
 --     name = Best School
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO 'first_table' ('id`, 'name') VALUES(89, 'Best School');-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
 -- The database name will be passed as an argument of the mysql command
 
-SELECT COUNT(*) FROM fist_table WHERE id=89;
+SELECT COUNT(*) FROM firt_table WHERE id=89;
