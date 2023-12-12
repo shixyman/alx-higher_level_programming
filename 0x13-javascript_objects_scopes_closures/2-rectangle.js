040a94de77622bf93a740a3d9b1b4b5db44a2281
shixyman <shixymanflowx@gmail.com>
2023-12-12 06:33:25 -0800

    modify

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // Create an empty object if w or h is not a positive integer
-      return {};
-    }
-
-    this.width = w;
-    this.height = h;
+	  if (w > 0 && h >0){
+		  this.width = w;
+		  this.height = h;
+	  }
   }
-}
+};
+
