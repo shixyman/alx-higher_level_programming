d3adb2c8465b48aa60079c354a7097e6de7e0805
shixyman <shixymanflowx@gmail.com>
2023-12-12 06:54:41 -0800

    modify

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,11 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h >0){
+	  if (w > 0 && h >0 && Number.isInteger(w) && Number.isInteger(h)){
 		  this.width = w;
 		  this.height = h;
+	  }else{
+		  class Rectangle{};
 	  }
   }
 };
