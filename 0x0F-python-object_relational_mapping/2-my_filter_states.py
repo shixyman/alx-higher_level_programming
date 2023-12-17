6ce3b9717fd6647704424e9a19bf30c330d1b06f
shixyman <shixymanflowx@gmail.com>
2023-12-16 17:12:44 -0800

    modify

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-python script that lists all states from the database hbtn_0e_0_usa with
-a given name
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute(f"SELECT * FROM states WHERE name LIKE '{argv[4]}' ORDER BY id ASC")
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
     for row in rows:
-        if row in rows:
-            print(row)
-    cursor.close()
-    db.close()
+        print(row)
+    cur.close()
