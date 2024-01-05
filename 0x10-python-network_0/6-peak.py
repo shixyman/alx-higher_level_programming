9173bdf6016a3a773c86b0778edbc11de1acc861
shixyman <shixymanflowx@gmail.com>
2024-01-05 06:44:08 -0800

    add files

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""contains the function find_peak"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    lint = list_of_integers
+    lgt = len(lint)
+    if lgt == 0:
+        return
+    m = lgt // 2
+    if (m == lgt - 1 or lint[m] >= lint[m + 1]) and (m == 0 or lint[m] >= lint[m - 1]):
+        return lint[m]
+    if m != lgt - 1 and lint[m + 1] > lint[m]:
+        return find_peak(lint[m + 1:])
+    return find_peak(lint[:m])
