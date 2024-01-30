682732fc47727030206875128bb7b64fa379c812
yousri rachid <shixymanflowx@gmail.com>
2024-01-30 13:20:30 +0100

    modify

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+
+const filePath = process.argv[2]; // Get the file path from command line arguments
+fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err); // Print the error object if an error occurred during reading
+	 } else {
+		console.log(data); // Print the content of the file
+		}
+	 });
