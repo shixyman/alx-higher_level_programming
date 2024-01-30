a81126f03676f86e2228f14cade2dea7f4fb9d3f
yousri rachid <shixymanflowx@gmail.com>
2024-01-30 13:34:24 +0100

    modify

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2]; // Get the file path from command line arguments
+const file = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err); // Print the error object if an error occurred during reading
-	 } else {
-		console.log(data); // Print the content of the file
-		}
-	 });
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
