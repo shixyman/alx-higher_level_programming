eb72b3b648788326049842258c44856f3b0ddb62
yousri rachid <shixymanflowx@gmail.com>
2024-01-30 13:24:46 +0100

    modify

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-
-
 const filePath = process.argv[2]; // Get the file path from command line arguments
+
 fs.readFile(filePath, 'utf-8', (err, data) => {
 	if (err) {
 		console.error(err); // Print the error object if an error occurred during reading
